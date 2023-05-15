13d3817aacebf0486dcfa5d4ee13c2d8c4fc3936
mac2 <mauricio.trevinon91@gmail.com>
2023-05-15 13:50:09 -0600

    Created a better organized folders in the project

---
+++ src/pages/experienceData/experience.js
@@ -0,0 +1,30 @@
+// experienceData.js
+const experienceData = [
+    {
+        title: "Experience",
+        information: [
+            {
+                name: "Cosmic Agency",
+                event: "Independent Consultant (2017 - present)",
+                description: "Experienced marketing and sales consultant adept at bringing brand visions to life. I identify growth opportunities, create landing pages and e-commerce solutions, and optimize user experience. Skilled in content creation, including video editing and written content. Proficient in Google and Meta Ads to boost traffic and visibility."
+            },
+            {
+                name: "Swappy Mexico",
+                event: "CEO & Co-Founder (2020 - 2022)",
+                description: "I led the development and growth of a second-hand clothes selling app startup. My responsibilities included conducting market research and data analysis to identify growth opportunities, representing the brand at events to build partnerships, and leading strategic decision-making processes."
+            },
+            {
+                name: "Royal Prestige 2015 - 2019",
+                event: "Direct Sales Lead (2015 - 2019)",
+                description: "As a high-level distributor in the Royal Prestige network, I consistently surpassed sales targets and was recognized as a top performer. I successfully expanded my sales team and customer base through strategic marketing initiatives, driving significant sales growth."
+            },
+            {
+                name: "The New Agency",
+                event: "Brand Manager (2013 - 2015)",
+                description: "I create effective branding strategies for clients, using my keen eye for detail and deep understanding of consumer behavior. I consistently deliver exceptional results, driving brand awareness, engagement, and revenue growth. Passionate about helping brands achieve their potential through social media, compelling content, and innovative marketing initiatives."
+            },
+        ]
+    }
+];
+
+export default experienceData;
