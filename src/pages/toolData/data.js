35f3e0aa457d204c0a1dfe3fe73011dcd1c86300
mac2 <mauricio.trevinon91@gmail.com>
2023-05-15 12:59:12 -0600

    Refactored tools page by creating a sectionData file and mapping thorugh it

---
+++ src/pages/toolData/data.js
@@ -0,0 +1,139 @@
+const sectionData = [
+    {
+        title: "Frameworks/Libraries",
+        information: [
+          {  
+            name: 'React',
+            description: "A powerful JavaScript library for building user interfaces."
+        }
+        ]
+    },
+        {
+          title: "Languages",
+          information: [
+            {
+              name: "HTML",
+              description: "A markup language used for creating web pages and applications."
+            },
+            {
+              name: "CSS",
+              description: "A style sheet language used for describing the presentation of web pages."
+            },
+            {
+              name: "JavaScript",
+              description: "A high-level programming language used for creating interactive web pages and applications."
+            },
+            {
+              name: "Node.js",
+              description: "A JavaScript runtime built on Chrome V8 JavaScript engine for building scalable network applications."
+            }
+          ]
+        },
+        {
+          title: "Design Tools",
+          information: [
+            {
+              name: "Figma",
+              description: "A collaborative interface design tool for creating and sharing interactive prototypes and design systems."
+            },
+            {
+              name: "Canva",
+              description: "An online graphic design platform for creating custom designs for print and web."
+            },
+            {
+              name: "Codux",
+              description: "A UI design tool for creating design systems and pattern libraries."
+            },
+            {
+              name: "Midjourney",
+              description: "A UX design tool for creating and testing user flows and prototypes."
+            }
+          ]
+        },
+        {
+          title: "Frameworks/Libraries",
+          information: [
+            {
+              name: "React",
+              description: "A powerful JavaScript library for building user interfaces."
+            }
+          ]
+        },
+        {
+          title: "CSS Frameworks",
+          information: [
+            {
+              name: "Tailwind CSS",
+              description: "A utility-first CSS framework for quickly building custom designs."
+            }
+          ]
+        },
+        {
+          title: "Version Control",
+          information: [
+            {
+              name: "Git",
+              description: "A distributed version control system for tracking changes in source code."
+            },
+            {
+              name: "GitHub",
+              description: "A web-based platform for hosting and collaborating on Git repositories."
+            }
+          ]
+        },
+        {
+          title: "Databases",
+          information: [
+            {
+              name: "MySQL",
+              description: "A popular open-source relational database management system."
+            }
+          ]
+        },
+        {
+          title: "Automation Tools",
+          information: [
+            {
+              name: "Zapier",
+              description: "A web-based automation tool for connecting apps and automating workflows."
+            },
+            {
+              name: "GPT",
+              description: "An AI language model for generating natural language text."
+            }
+          ]
+        },
+        {
+          title: "Marketing Tools",
+          information: [
+            {
+              name: "Klaviyo",
+              description: "An email marketing and SMS platform for driving customer engagement and revenue growth."
+            },
+            {
+              name: "Mailchimp",
+              description: "An all-in-one marketing platform for creating and automating email campaigns, social ads, landing pages, and more."
+            },
+            {
+              name: "Google Ads & Meta Ads",
+              description: "Advertising platforms for driving website traffic and conversions through paid search and display advertising."
+            }
+          ]
+        },
+        {
+          title: "Selling Tools",
+          information: [
+            {
+              name: "Salesforce",
+              description: "A cloud-based CRM platform for managing sales, customer service, and marketing automation."
+            },
+            {
+              name: "HubSpot",
+              description: "A CRM and marketing automation platform for managing and scaling inbound marketing campaigns."
+            }
+          ]
+        }
+    ]
+      
+
+export default sectionData
