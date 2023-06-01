c1147eacbe9425853d2a3a28d8e893b3ffd93292
Mauricio-tech <mauricio.trevinon91@gmail.com>
2023-06-01 15:27:00 -0600

    Updated info

--- src/lib/project.js
+++ src/lib/project.js
@@ -1,19 +1,24 @@
 const projectsData = [
-  {
-      title: "Projects",
-      information: [
-          {
-              name: "LocalEats",
-              description: "Local Eats is a web application that allows users to post their favorite selfmade dishes along with information about where the dish can be bought, the time when the dish is available, and the price. The application is built using HTML, CSS (tailwind), javascript, Node.js, Express.js, SQL, Sequelize, Bcrypt for password security, and uses the Geolocation API of the browser.",
-              link: { href: 'https://github.com/mauri-tech/localeats', label: 'github.com/mauri-tech/localeats' },
-          },
-          {
-              name: "ThermoFoods",
-              description: "This project focuses on helping people control their weight by selecting healthy recipes that are appropriate for the current weather. Using two APIs, one for recipes and the other for obtaining the current temperature using geolocation, the application provides options for light and fresh meals on hot days and options for more caloric meals on cold days.",
-              link: { href: 'https://github.com/mauri-tech/ThermoFood-App', label: 'github.com/mauri-tech/ThermoFood-App' },
-          }
-      ]
-  }
-]
+    {
+    title: "Projects",
+    information: [
+    {
+    name: "Resumate",
+    description: "Resumate is a revolutionary resume creation platform powered by AI. It allows users to effortlessly create standout resumes by leveraging OpenAI's key. The project involved building the front-end and back-end using React and Node.js, with GraphQL and Apollo for efficient data management.",
+    link: { href: 'https://github.com/mauri-tech/resumate', label: 'github.com/your-username/resumate' },
+    },
+    {
+    name: "ThermoFoods",
+    description: "This project focuses on helping people control their weight by selecting healthy recipes that are appropriate for the current weather. Using two APIs, one for recipes and the other for obtaining the current temperature using geolocation, the application provides options for light and fresh meals on hot days and options for more caloric meals on cold days.",
+    link: { href: 'https://github.com/mauri-tech/ThermoFood-App', label: 'github.com/mauri-tech/ThermoFood-App' },
+    },
+    {
+    name: "LocalEats",
+    description: "Local Eats is a web application that allows users to post their favorite self-made dishes along with information about where the dish can be bought, the time when the dish is available, and the price. The application is built using HTML, CSS (Tailwind), JavaScript, Node.js, Express.js, SQL, Sequelize, Bcrypt for password security, and uses the Geolocation API of the browser.",
+    link: { href: 'https://github.com/mauri-tech/localeats', label: 'github.com/mauri-tech/localeats' },
+    }
+    ]
+    }
+    ];
 
 export default projectsData
